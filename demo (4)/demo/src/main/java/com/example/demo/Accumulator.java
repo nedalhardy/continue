f13e5becbd49f64f01c05f4fdcf0799b6f6ed783
Nedal jedidi <nedal.jed@gmail.com>
2019-04-14 18:15:26 +0200

    projet

---
+++ demo (4)/demo/src/main/java/com/example/demo/Accumulator.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Accumulator {
+    private String sum;
+    private String avg;
+    private String count;
+}
---
+++ demo (4)/demo/src/main/java/com/example/demo/Service.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Service {
+    public void check(Test test){
+        switch (test.getData()){
+            case "sum":  break;
+        }
+    }
+}
